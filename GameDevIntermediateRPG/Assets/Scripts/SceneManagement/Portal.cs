43b966cf01efee5dae4dfc1a7c0d9ec3bed73baa
MrBowser <mitchbowser1990@gmail.com>
2023-05-19 19:34:15 -0700

    5/19 edits

---
+++ GameDevIntermediateRPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int SceneIndexToLoad =-1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag =="Player")
+            {
+                 
+
+                SceneManager.LoadScene(SceneIndexToLoad);
+            }
+        }
+    }
+}
