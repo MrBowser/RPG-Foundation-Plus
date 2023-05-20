3a1af37880f556bc9417c58fe477e587d7a62a1b
MrBowser <mitchbowser1990@gmail.com>
2023-05-20 09:10:27 -0700

    5/20 edits

--- GameDevIntermediateRPG/Assets/Scripts/SceneManagement/Portal.cs
+++ GameDevIntermediateRPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -9,15 +9,53 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int SceneIndexToLoad =-1;
+        [SerializeField] Transform SpawnPoint;
+
+        Portal otherPortal;
 
         private void OnTriggerEnter(Collider other)
         {
             if(other.tag =="Player")
             {
-                 
 
-                SceneManager.LoadScene(SceneIndexToLoad);
+                StartCoroutine(Transition());
+                
             }
         }
+
+        private IEnumerator Transition()
+        {
+            
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(SceneIndexToLoad);
+
+            otherPortal = GetOtherPortal();
+
+            UpdatePlayer(otherPortal);
+            
+            Destroy(gameObject);
+        }
+
+
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) { continue; }
+
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.transform.position = otherPortal.SpawnPoint.position;
+            player.transform.rotation = otherPortal.SpawnPoint.rotation;
+
+        }
+       
     }
 }
